1865449f424dd7c3da39b6eaa3f369814bcfba92
Fernando Correa de Oliveira <fernandocorrea@gmail.com>
2019-06-07 19:35:53 +0100

    Initial Commit

---
+++ QuitSmoking/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  QuitSmoking
+//
+//  Created by Fernando Oliveira on 07/06/19.
+//  Copyright © 2019 Fernando Oliveira. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ QuitSmokingTests/QuitSmokingTests.swift
@@ -0,0 +1,34 @@
+//
+//  QuitSmokingTests.swift
+//  QuitSmokingTests
+//
+//  Created by Fernando Oliveira on 07/06/19.
+//  Copyright © 2019 Fernando Oliveira. All rights reserved.
+//
+
+import XCTest
+@testable import QuitSmoking
+
+class QuitSmokingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
